27056efaf88e93b992768b72b5be4d9091e714af
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2023-11-30 22:01:50 +0000

    All changes made during class

--- profile.js
+++ profile.js
@@ -7,10 +7,13 @@ let main = document.querySelector(".main");
 button.addEventListener("click", function() {
 
     // 1. Updated the innerHTML of profileName to your name or a made up one.
+  profileName.innerHTML = "Joshua Federman";
 
     // 2. Updated the src of profileImage a picture of you or one that makes sense.
+  profileImage.src = "https://stem.ucf.edu/wp-content/uploads/2022/11/Joshua-Federman-scaled.jpeg";
 
     // 3. Updated the src of main to a photo that's be your first post.
+  main.src = "https://cdn.vox-cdn.com/thumbor/YAPRoT9zjlGPiogNN1qeguHwDoo=/0x165:1000x915/1200x800/filters:focal(0x165:1000x915)/cdn.vox-cdn.com/uploads/chorus_image/image/46800412/_haveseen_shutterstock_130167068.0.0.jpg";
 
 
 });
--- shapes.js
+++ shapes.js
@@ -2,6 +2,7 @@
 let circle = document.querySelector(".circle");
 let square = document.querySelector(".square");
 let rectangle = document.querySelector(".rectangle");
+let isOrange = false;
 
 circle.addEventListener("click", function() {
 
@@ -9,8 +10,10 @@ circle.addEventListener("click", function() {
     // - Set the background color to purple.
     // - Set the width to 150px.
     // - Set the height to 150px.
-   
-
+  circle.style.backgroundColor = "purple";
+  circle.style.width = 150;
+  circle.style.height = 150;
+  circle.style.opacity = 0.4;
 });
 
 square.addEventListener("click", function() {
@@ -19,9 +22,12 @@ square.addEventListener("click", function() {
     // - Set the background color to yellow.
     // - Set the width to 150px.
     // - Set the height to 150px.
-
-
-
+  square.style.backgroundColor = "yellow";
+  square.style.width = 150;
+  square.style.height = 150;
+  square.style.borderStyle = "solid";
+  square.style.border = 5;
+  square.style.borderColor = "red";
 });
 
 rectangle.addEventListener("click", function() {
@@ -30,9 +36,15 @@ rectangle.addEventListener("click", function() {
     // - Set the background color to orange.
     // - Set the width to 250px.
     // - Set the height to 150px.
-    
-
-
+  if (!isOrange) {
+    rectangle.style.backgroundColor = "orange";
+    rectangle.style.width = 250;
+    rectangle.style.height = 150;
+    isOrange = true;
+  }
+  else {
+    rectangle.style.display = "none";
+  }
 });
 
 // Extension: Check out the available CSS properties here: https://www.w3schools.com/cssref/index.php 
